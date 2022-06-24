96059e342d3321bdae979174de3dc9088618860e
SeyaKen <75517657+SeyaKen@users.noreply.github.com>
2022-06-24 10:39:56 +0900

    first commit

---
+++ lib/Othersprofile/others_profile.dart
@@ -0,0 +1,205 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:eigo/profile/answers.dart';
+import 'package:eigo/profile/quesions.dart';
+import 'package:eigo/services/database.dart';
+
+class OthersProfile extends StatefulWidget {
+  OthersProfile({Key? key, required this.othersUid}) : super(key: key);
+  String othersUid;
+
+  @override
+  State<OthersProfile> createState() => _OthersProfileState(othersUid);
+}
+
+class _OthersProfileState extends State<OthersProfile> {
+  String othersUid;
+  Stream<QuerySnapshot<Object?>>? profileListsStream;
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+  bool switchColor0 = true;
+  bool switchColor1 = false;
+
+  _OthersProfileState(this.othersUid);
+
+  getHomeLists() async {
+    profileListsStream = await DatabaseService(othersUid).fetchImage();
+    setState(() {});
+  }
+
+  onScreenLoaded() async {
+    getHomeLists();
+  }
+
+  @override
+  void initState() {
+    onScreenLoaded();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<QuerySnapshot>(
+        stream: profileListsStream,
+        builder: (context, snapshot) {
+          return snapshot.hasData
+              ? Scaffold(
+                  key: _scaffoldKey,
+                  appBar: AppBar(
+                    automaticallyImplyLeading: false,
+                    title: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        InkWell(
+                          onTap: () {
+                            Navigator.pop(
+                              context,
+                            );
+                          },
+                          child: const Icon(
+                            Icons.arrow_back_ios_new_rounded,
+                            color: Colors.black,
+                          ),
+                        ),
+                      ],
+                    ),
+                    backgroundColor: Colors.transparent,
+                    elevation: 0,
+                  ),
+                  resizeToAvoidBottomInset: false,
+                  body: Column(
+                    children: [
+                      SizedBox(
+                        width: MediaQuery.of(context).size.width * 0.85,
+                        child: Row(
+                          children: [
+                            ClipRRect(
+                              borderRadius: BorderRadius.circular(150.0),
+                              child: SizedBox(
+                                width: 110,
+                                height: 110,
+                                child: Image.network(
+                                  snapshot.data!.docs[0]['ProfilePicture'] == ''
+                                      ? 'https://firebasestorage.googleapis.com/v0/b/eigo-224ae.appspot.com/o/44884218_345707102882519_2446069589734326272_n.jpeg?alt=media&token=9cdb200e-9d19-4657-a861-711f87ad10f4'
+                                      : snapshot.data!.docs[0]
+                                          ['ProfilePicture'],
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                            ),
+                            SizedBox(
+                                width:
+                                    MediaQuery.of(context).size.width * 0.05),
+                            Expanded(
+                              child: Text(
+                                snapshot.data!.docs[0]['name'],
+                                style: const TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 25,
+                                ),
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          SizedBox(
+                              width: MediaQuery.of(context).size.width,
+                              height:
+                                  MediaQuery.of(context).size.height * 0.02),
+                          SizedBox(
+                            width: MediaQuery.of(context).size.width * 0.9,
+                            child: Text(
+                              snapshot.data!.docs[0]['selfIntroduction'],
+                              style: const TextStyle(
+                                fontSize: 19,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          const SizedBox(height: 10),
+                          Row(children: [
+                            InkWell(
+                              onTap: () {
+                                setState(() {
+                                  switchColor0 = !switchColor0;
+                                  switchColor1 = !switchColor1;
+                                });
+                              },
+                              child: Container(
+                                padding:
+                                    const EdgeInsets.symmetric(vertical: 10),
+                                decoration: BoxDecoration(
+                                  border: Border(
+                                    bottom: BorderSide(
+                                      color: switchColor0
+                                          ? Colors.black
+                                          : Colors.grey,
+                                      width: switchColor0 ? 2.0 : 1.0,
+                                    ),
+                                  ),
+                                ),
+                                width: MediaQuery.of(context).size.width * 0.5,
+                                child: const Center(
+                                  child: Text('質問',
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 16,
+                                      )),
+                                ),
+                              ),
+                            ),
+                            InkWell(
+                              onTap: () {
+                                setState(() {
+                                  switchColor0 = !switchColor0;
+                                  switchColor1 = !switchColor1;
+                                });
+                              },
+                              child: Container(
+                                padding:
+                                    const EdgeInsets.symmetric(vertical: 10),
+                                decoration: BoxDecoration(
+                                  border: Border(
+                                    bottom: BorderSide(
+                                      color: switchColor1
+                                          ? Colors.black
+                                          : Colors.grey,
+                                      width: switchColor1 ? 2.0 : 1.0,
+                                    ),
+                                  ),
+                                ),
+                                width: MediaQuery.of(context).size.width * 0.5,
+                                child: const Center(
+                                  child: Text('回答',
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 16,
+                                      )),
+                                ),
+                              ),
+                            ),
+                          ])
+                        ],
+                      ),
+                      // ここに質問のリストを載せる
+                      Flexible(
+                        child: SizedBox(
+                          height: MediaQuery.of(context).size.width,
+                          child: switchColor0
+                              ? Questions(othersUid)
+                              : Answers(othersUid),
+                        ),
+                      ),
+                    ],
+                  ),
+                )
+              : const Center(child: CircularProgressIndicator());
+        });
+  }
+}
---
+++ lib/main_page.dart
@@ -0,0 +1,76 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:eigo/Home/home_screen.dart';
+import 'package:eigo/profile/profile_page.dart';
+
+class MainPage extends StatefulWidget {
+  MainPage({Key? key, required this.currenttab}) : super(key: key);
+
+  int currenttab;
+
+  @override
+  _MainPageState createState() => _MainPageState(currenttab);
+}
+
+class _MainPageState extends State<MainPage> {
+  int currenttab;
+  _MainPageState(this.currenttab);
+  String? myUserUid;
+
+  getMyUserUid() async {
+    myUserUid = FirebaseAuth.instance.currentUser!.uid;
+    setState(() {});
+  }
+
+  onScreenLoaded() async {
+    await getMyUserUid();
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    onScreenLoaded();
+    setState(() {});
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+          onTap: (index) {
+            setState(() {
+              currenttab = index;
+            });
+          },
+          currentIndex: currenttab,
+          type: BottomNavigationBarType.fixed,
+          selectedItemColor: const Color(0xff00376b),
+          backgroundColor: Colors.white,
+          unselectedItemColor: Colors.grey,
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.home,
+                size: 30,
+              ),
+              label: 'ホーム',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.account_circle,
+                size: 30,
+              ),
+              label: 'プロフィール',
+            ),
+          ]),
+      body: IndexedStack(
+        children: const [
+          HomeScreen(),
+          ProfilePage(),
+        ],
+        index: currenttab,
+      ),
+    );
+  }
+}
