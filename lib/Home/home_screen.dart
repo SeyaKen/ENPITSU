12fc524e7ccc362856cb313e7a1d159d3b339beb
SeyaKen <75517657+SeyaKen@users.noreply.github.com>
2022-07-21 19:53:55 +0900

    whoooooooooooooooooooo

--- lib/Home/home_screen.dart
+++ lib/Home/home_screen.dart
@@ -40,6 +40,23 @@ class _HomeScreenState extends State<HomeScreen> {
     setState(() {});
   }
 
+  final BannerAdListener _adLister = BannerAdListener(
+    // Called when an ad is successfully received.
+    onAdLoaded: (Ad ad) => print('Ad loaded.'),
+    // Called when an ad request failed.
+    onAdFailedToLoad: (Ad ad, LoadAdError error) {
+      // Dispose the ad here to free resources.
+      ad.dispose();
+      print('Ad failed to load: $error');
+    },
+    // Called when an ad opens an overlay that covers the screen.
+    onAdOpened: (Ad ad) => print('Ad opened.'),
+    // Called when an ad removes an overlay that covers the screen.
+    onAdClosed: (Ad ad) => print('Ad closed.'),
+    // Called when an impression occurs on the ad.
+    onAdImpression: (Ad ad) => print('Ad impression.'),
+  );
+
   @override
   void initState() {
     super.initState();
@@ -50,7 +67,7 @@ class _HomeScreenState extends State<HomeScreen> {
           adUnitId: adState.bannerAdUnitId,
           size: AdSize.banner,
           request: const AdRequest(),
-          listener: adState.,
+          listener: const  BannerAdListener(),
         );
       });
     });
