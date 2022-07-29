48e282a88622c3f4875272af251f717ce8cb0a55
SeyaKen <75517657+SeyaKen@users.noreply.github.com>
2022-07-29 18:59:01 +0900

    first second second second commit

--- lib/TextsTimer/record_time_widow.dart
+++ lib/TextsTimer/record_time_widow.dart
@@ -9,9 +9,8 @@ class RecordTimeWindow extends StatefulWidget {
 }
 
 class _RecordTimeWindowState extends State<RecordTimeWindow> {
-  int _selectedFruit = 0;
+  int? hour;
 
-  // This shows a CupertinoModalPopup with a reasonable fixed height which hosts CupertinoPicker.
   void _showDialog(Widget child) {
     showCupertinoModalPopup<void>(
         context: context,
@@ -22,31 +21,21 @@ class _RecordTimeWindowState extends State<RecordTimeWindow> {
             margin: EdgeInsets.only(
               bottom: MediaQuery.of(context).viewInsets.bottom,
             ),
-            // Provide a background color for the popup.
             color: CupertinoColors.systemBackground.resolveFrom(context),
-            // Use a SafeArea widget to avoid system overlaps.
             child: Column(
               children: [
                 CupertinoTimerPicker(
-                  
                   mode: CupertinoTimerPickerMode.hm,
                   onTimerDurationChanged: (value) {
-                    
+                    print(value.toString().length);
+                    print(
+                        'hour:${value.toString().length == 14 ? value.toString().substring(0, 1) : value.toString().substring(0, 2)}');
+                    print('minutes:${value.toString().length == 14 ? value.toString().substring(2, 4) : value.toString().substring(3, 5)}');
                   },
                 ),
               ],
             )));
   }
-
-  double _kItemExtent = 32.0;
-  List<String> _fruitNames = <String>[
-    'Apple',
-    'Mango',
-    'Banana',
-    'Orange',
-    'Pineapple',
-    'Strawberry',
-  ];
 
   @override
   Widget build(BuildContext context) {
@@ -68,36 +57,14 @@ class _RecordTimeWindowState extends State<RecordTimeWindow> {
           ),
           CupertinoButton(
             padding: EdgeInsets.zero,
-            // Display a CupertinoPicker with list of fruits.
             onPressed: () => _showDialog(
-              CupertinoPicker(
-                magnification: 1.22,
-                squeeze: 1.2,
-                useMagnifier: true,
-                itemExtent: _kItemExtent,
-                // This is called when selected item is changed.
-                onSelectedItemChanged: (int selectedItem) {
-                  setState(() {
-                    _selectedFruit = selectedItem;
-                  });
-                },
-                children:
-                    List<Widget>.generate(_fruitNames.length, (int index) {
-                  return Center(
-                    child: Text(
-                      _fruitNames[index],
-                    ),
-                  );
-                }),
+              Container(
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height,
+                color: Colors.black,
               ),
             ),
-            // This displays the selected fruit name.
-            child: Text(
-              _fruitNames[_selectedFruit],
-              style: const TextStyle(
-                fontSize: 22.0,
-              ),
-            ),
+            child: Text('$hour'),
           )
         ],
       ),
